a517ccc67a4b2454db974654b5c7b34e9b321876
gchan82 <42255277+gchan82@users.noreply.github.com>
2019-02-19 00:00:18 -0800

    first commit

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+  int a, b, sum;
+  cout << "enter a:\n";
+  cin >> a;
+  cout << "enter b:\n";
+  cin >> b;
+  sum = a + b;
+  cout << "your sum " << sum << endl;
+  return 0;
+}
